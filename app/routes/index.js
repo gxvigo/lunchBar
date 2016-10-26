74c0b1954f552e65bb2874e5338be2f8d9b6f434
gxvigo <gxvigo@gmail.com>
2016-10-27 00:16:45 +1300

    properly loaded Bootstrap, started menu table

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res){
-    res.render('index'); 
+    res.render('index'); // this load ./views/index.ejs
 });
 
 module.exports = router;
