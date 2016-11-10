b3d17cc89eb749f7635f3f0830bcec9356eaeec6
gxvigo <gxvigo@gmail.com>
2016-11-10 23:49:13 +1300

    Added tooltip for currentTermMeals dietary restrictions

--- app/public/js/mealsTable.js
+++ app/public/js/mealsTable.js
@@ -1,3 +1,8 @@
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip(); 
+});
+
+
 $(function() {
     $('.meal-table-content').on('click', function(e){
         console.log("clicked!");
